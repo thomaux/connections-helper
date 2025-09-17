93b8bab7f3e5868b52b9b0a22a727ed213d8a309
Thomas Anciaux <1404536+thomaux@users.noreply.github.com>
2025-09-17 09:41:52 +0200

    improve mobile responsiveness

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
 
 const root = document.createElement("div");
+root.style.setProperty("max-width", "800px");
+root.style.setProperty("margin", "auto");
 document.body.appendChild(root);
 
 createRoot(root).render(
