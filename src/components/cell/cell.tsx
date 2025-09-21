be12060f0d9c785536054122aa4f9a2233b178e5
Thomas Anciaux <1404536+thomaux@users.noreply.github.com>
2025-09-21 11:31:02 +0200

    Disable board when no active color is present

--- src/components/cell/cell.tsx
+++ src/components/cell/cell.tsx
@@ -3,15 +3,25 @@ import React from "react";
 import type { NamedColor } from "../../types/named-color.ts";
 import { Button } from "../button/button.tsx";
 import styles from "./cell.module.scss";
-import { useAppDispatch } from "../../store/hooks.ts";
+import { useAppDispatch, useAppSelector } from "../../store/hooks.ts";
 import { toggleActiveColor } from "../../store/features/helper/helperSlice.ts";
+import type { RootState } from '../../store/store.ts';
 
 type CellProps = {
   word: string;
   colors: NamedColor[];
 };
 
+function selectColorsState({ helper }: RootState) {
+  return {
+    hasActiveColor: helper.colors.some(color => color.colorState === 'active'),
+    lockedColors: helper.colors.filter(color => color.colorState === 'locked').map(color => color.name),
+  }
+}
+
 export function Cell({ word, colors }: CellProps) {
+  const { hasActiveColor, lockedColors } = useAppSelector(selectColorsState);
+  const hasLockedColor = colors.some(color => lockedColors.includes(color));
   const dispatch = useAppDispatch();
 
   return (
@@ -19,6 +29,7 @@ export function Cell({ word, colors }: CellProps) {
       className={styles.Cell}
       colors={colors}
       onClick={() => dispatch(toggleActiveColor({ word }))}
+      isDisabled={!hasActiveColor || hasLockedColor}
     >
       {word}
     </Button>
